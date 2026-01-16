bc631c12db60ea3fdb810e2d7a333e13c28d29d7
Vigneshwar <vigneshwar.s1996@gmail.com>
2026-01-17 01:06:19 +0530

    Add conditional rendering for movie poster in Moviebox component

--- movie-app/src/component/Moviebox.jsx
+++ movie-app/src/component/Moviebox.jsx
@@ -6,7 +6,10 @@ export default function Moviebox({movie}) {
         <div className='movie-box-inner'>
             <div className='movie-image'>
                 <button className='movie-fav-icon'> ❤︎ </button>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                {movie.poster_path !== null 
+                ?<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                :<img src="https://picsum.photos/seed/picsum/500/750" alt={movie.title} />}
+                
             </div>
             <div className='movie-name'>{movie.title}</div>
             <div className='movie-year'>{movie.release_date.split("-")[0]}</div>
